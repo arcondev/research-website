b096e0391e75b35b5674044533440d56869498d8
Saurav Tuladhar <saurav@arcon.com>
2020-09-29 06:51:41 -0400

    Add plugin, theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,16 +19,15 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Arcon Corporation', 'https://www.arcon.com/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Github', 'https://www.github.com/arconcorporation'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+THEME='bootstrap'
+PLUGINS = ['pelican.plugins.render_math']
